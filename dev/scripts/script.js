2cb9623bbbd3ce3e164eecd93db16c067b1e3bc7
DmitryKryukov <demobbtw@gmail.com>
2022-04-11 21:31:06 +0400

    Add basic project hover animation

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const MAXSKILLSROTATION = 7;
-    const ROTATE = false;
-    if (ROTATE) {
-        const skillItems = document.querySelectorAll(".skills-item").forEach((skillItem) => {
-            const randomRotation = Math.floor(Math.random() * MAXSKILLSROTATION * 2 - MAXSKILLSROTATION);
-            console.log(randomRotation);
-            skillItem.style.transform = `rotate(${randomRotation}deg)`;
-        })
-    }
-}
-) 
+document.addEventListener('DOMContentLoaded', () => {
+  const MAXSKILLSROTATION = 5
+  const ROTATE = true
+  if (ROTATE) {
+    const skillItems = document
+      .querySelectorAll('.skills-item')
+      .forEach((skillItem) => {
+        const randomRotation = Math.floor(
+          Math.random() * MAXSKILLSROTATION * 2 - MAXSKILLSROTATION
+        )
+        console.log(randomRotation)
+        skillItem.style.transform = `rotate(${randomRotation}deg)`
+      })
+  }
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,104 +1,120 @@
-const { src, dest, parallel, series, watch } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp')
 
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create()
 
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify-es').default
 
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const cleancss = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'))
+const autoprefixer = require('gulp-autoprefixer')
+const cleancss = require('gulp-clean-css')
 
-const imagecomp = require('compress-images');
-const del = require('del');
+const imagecomp = require('compress-images')
+const del = require('del')
 
-const pug = require('gulp-pug');
+const pug = require('gulp-pug')
 
 function browsersync() {
-    browserSync.init({
-        server: { baseDir: 'dev/' },
-        notify: false,
-        online: true
-    })
+  browserSync.init({
+    server: { baseDir: 'dev/' },
+    notify: false,
+    online: true,
+  })
 }
 
 function scripts() {
-	return src(['dev/scripts/script.js'])
-	.pipe(concat('script.min.js'))
-	.pipe(uglify())
-	.pipe(dest('dev/scripts/'))
-	.pipe(browserSync.stream())
+  return src(['dev/scripts/script.js'])
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(dest('dev/scripts/'))
+    .pipe(browserSync.stream())
 }
 
 function styles() {
-	return src('dev/scss/style.scss')
-	.pipe(sass())
-	.pipe(concat('style.min.css'))
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(cleancss( { level: { 1: { specialComments: 0 } }, format: 'beautify'} ))
-	.pipe(dest('dev/css/'))
-	.pipe(browserSync.stream())
+  return src('dev/scss/style.scss')
+    .pipe(sass())
+    .pipe(concat('style.min.css'))
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })
+    )
+    .pipe(
+      cleancss({ level: { 1: { specialComments: 0 } }, format: 'beautify' })
+    )
+    .pipe(dest('dev/css/'))
+    .pipe(browserSync.stream())
 }
 
 async function images() {
-	imagecomp(
-		"dev/images/src/**/*",
-		"dev/images/opt/",
-		{ compress_force: false, statistic: true, autoupdate: true }, false,
-		{ jpg: { engine: "mozjpeg", command: ["-quality", "80"] } },
-		{ png: { engine: "pngquant", command: ["--quality=80-100", "-o"] } },
-		{ svg: { engine: "svgo", command: "--multipass" } },
-		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
-		function (err, completed) {
-			if (completed === true) {
-				browserSync.reload()
-			}
-		}
-	)
+  imagecomp(
+    'dev/images/src/**/*',
+    'dev/images/opt/',
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: 'mozjpeg', command: ['-quality', '80'] } },
+    { png: { engine: 'pngquant', command: ['--quality=80-100', '-o'] } },
+    { svg: { engine: 'svgo', command: '--multipass' } },
+    {
+      gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] },
+    },
+    function (err, completed) {
+      if (completed === true) {
+        browserSync.reload()
+      }
+    }
+  )
 }
 
 function cleanimg() {
-	return del('dev/images/opt/**/*', { force: true })
+  return del('dev/images/opt/**/*', { force: true })
 }
 
 function views() {
-    return src('dev/pug/*.pug')
+  return src('dev/pug/*.pug')
     .pipe(
       pug({
-		pretty: true
-	})
+        pretty: true,
+      })
     )
-    .pipe(dest('dev'));
+    .pipe(dest('dev'))
 }
 
 function startwatch() {
-	watch(['dev/**/*.js', '!dev/**/*.min.js'], scripts);
-    watch('dev/**/*.scss', styles);
-    watch('dev/**/*.pug', views).on('change', browserSync.reload);
-    watch('dev/images/src/**/*', images);
+  watch(['dev/**/*.js', '!dev/**/*.min.js'], scripts)
+  watch('dev/**/*.scss', styles)
+  watch('dev/**/*.pug', views).on('change', browserSync.reload)
+  watch('dev/images/src/**/*', images)
 }
 
 function build() {
-	return src([
-		'dev/css/**/*.min.css',
-		'dev/scripts/**/*.min.js',
-		'dev/images/opt/**/*',
-		'dev/fonts/*',
-		'dev/*.html'
-		], { base: 'dev' })
-	.pipe(dest('dist'))
+  return src(
+    [
+      'dev/css/**/*.min.css',
+      'dev/scripts/**/*.min.js',
+      'dev/images/opt/**/*',
+      'dev/fonts/*',
+      'dev/*.html',
+    ],
+    { base: 'dev' }
+  ).pipe(dest('dist'))
 }
 function cleandist() {
-	return del('dist/**/*', { force: true })
+  return del('dist/**/*', { force: true })
 }
 
-exports.browsersync = browsersync;
-exports.views = views;
-exports.scripts = scripts;
-exports.styles = styles;
-exports.images = images;
-exports.cleanimg = cleanimg;
-exports.cleandist = cleandist;
+exports.browsersync = browsersync
+exports.views = views
+exports.scripts = scripts
+exports.styles = styles
+exports.images = images
+exports.cleanimg = cleanimg
+exports.cleandist = cleandist
 
-exports.build = series(cleandist, views, styles, scripts, images, build);
-exports.default = parallel(views, styles, scripts, browsersync, startwatch);
+exports.build = series(cleandist, views, styles, scripts, images, build)
+exports.default = parallel(
+  views,
+  styles,
+  scripts,
+  images,
+  browsersync,
+  startwatch
+)
