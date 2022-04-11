e19bdce121e94f866db88661d0c41bb4dff47e2a
DmitryKryukov <demobbtw@gmail.com>
2022-04-11 23:20:21 +0400

    Add project animation draft

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -8,8 +8,31 @@ document.addEventListener('DOMContentLoaded', () => {
         const randomRotation = Math.floor(
           Math.random() * MAXSKILLSROTATION * 2 - MAXSKILLSROTATION
         )
-        console.log(randomRotation)
         skillItem.style.transform = `rotate(${randomRotation}deg)`
       })
   }
+
+  const leftHand = document.querySelector('.project-hover-left')
+  const rightHand = document.querySelector('.project-hover-right')
+
+  const project = document.querySelector('.project')
+  let projectHalfWidth = project.offsetWidth / 2
+
+  window.addEventListener('resize', () => {
+    projectHalfWidth = project.offsetWidth / 2
+  })
+
+  project.addEventListener('mousemove', (e) => {
+    const relativeCoordX = Math.abs(
+      Math.abs(projectHalfWidth - e.offsetX) - projectHalfWidth
+    )
+    const factorX = relativeCoordX / projectHalfWidth
+    const x = relativeCoordX - leftHand.offsetWidth * factorX
+    const y = e.offsetY - leftHand.offsetHeight / 2
+
+    leftHand.style.setProperty('--x-position', x + 'px')
+    leftHand.style.setProperty('--y-position', y + 25 + 'px')
+    rightHand.style.setProperty('--x-position', x + 'px')
+    rightHand.style.setProperty('--y-position', y + 'px')
+  })
 })
