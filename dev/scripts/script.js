89e293307b4cf52262fbc832e1696d7b76971872
DmitryKryukov <demobbtw@gmail.com>
2022-04-20 22:04:01 +0400

    Add and hide hand rotation aniimation

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const rightHand = document.getElementById('js-hand-right')
   const project = document.getElementById('js-project-hand')
   let projectHalfWidth = project.offsetWidth / 2
+
   window.addEventListener('resize', () => {
     projectHalfWidth = project.offsetWidth / 2
   })
@@ -22,14 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const relativeCoordX = Math.abs(
       Math.abs(projectHalfWidth - e.offsetX) - projectHalfWidth
     )
-    const factorPi = Math.sin((Math.PI * e.offsetX) / (project.offsetWidth * 2 - projectHalfWidth))
+    const factorPi = Math.sin(
+      (Math.PI * e.offsetX) / (project.offsetWidth * 2 - projectHalfWidth)
+    )
     const factor = relativeCoordX / projectHalfWidth
     const x = (relativeCoordX - 450 * factor) * factorPi
     const y = e.offsetY - leftHand.offsetHeight / 2
 
     leftHand.style.setProperty('--x-position', x + 'px')
     leftHand.style.setProperty('--y-position', y + 25 + 'px')
+
     rightHand.style.setProperty('--x-position', x + 'px')
     rightHand.style.setProperty('--y-position', y + 'px')
+
+    //Хотел добавить поворот руки при движении, но рендеринг браузера некорректно работает с backdrop-filter
+    //const rotation = (1 - factor) * 20
+    //leftHand.style.setProperty('--rotation', -rotation + 'deg')
+    //rightHand.style.setProperty('--rotation', rotation + 'deg')
   })
 })
